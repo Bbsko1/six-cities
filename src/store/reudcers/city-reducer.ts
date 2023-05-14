df67cbebc264bc43cb132f80409e063f9fd83ba4
Bbsko <artmq4@gmail.com>
2023-05-14 23:47:31 +0500

    cityReducer переписан на RTK

--- src/store/reudcers/city-reducer.ts
+++ src/store/reudcers/city-reducer.ts
@@ -1,18 +1,19 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { ActionType, LocationActions } from "../../types/card-actions";
 import { CityState } from "../../types/types";
+import { addCities, changeActiveCity } from "../actions/card-actions";
 
 const initialState: CityState = {
     cities: [],
     activeCity: null,
 }
 
-export const cityReducer = (state: CityState = initialState, action: LocationActions): CityState => {
-    switch (action.type) {
-        case ActionType.AddCities:
-            return {...state, cities: action.payload};
-        case ActionType.ChangeCity:
-            return {...state, activeCity: action.payload};
-        default:
-            return state;
-    }
-}
+export const cityReducer = createReducer(initialState, (builder) => {
+    builder
+        .addCase(addCities, (state, action) => {
+            state.cities = action.payload;
+        })
+        .addCase(changeActiveCity, (state, action) => {
+            state.activeCity = action.payload;
+        })
+});
