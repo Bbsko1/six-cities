b02fdf04aa8d84c058420c101923957c3d5746de
Bbsko <artmq4@gmail.com>
2023-05-16 22:28:11 +0500

    удален старый user reducer

--- src/store/reudcers/user-reducer.ts
+++ src/store/reudcers/user-reducer.ts
@@ -1,23 +1,11 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { AuthorizationStatus } from "../../const";
-import { ActionType, LocationActions } from "../../types/card-actions";
 import { UserData, UserState } from "../../types/types";
 import { fetchCheckAuth, loginAction, logoutAction } from "../actions/card-actions";
 
 const initialState: UserState = {
     authStatus: AuthorizationStatus.Unknown,
     userData: null,
-}
-
-export const userReducer = (state: UserState = initialState, action: LocationActions): UserState => {
-    switch (action.type) {
-        case ActionType.RequireAuth:
-            return {...state, authStatus: action.payload};
-        case ActionType.ChangeUserData:
-            return {...state, userData: action.payload};
-        default:
-            return state;
-    }
 }
 
 export const userSlice = createSlice({
